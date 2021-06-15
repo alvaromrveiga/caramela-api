fef40fbf55c3c046bebb448f5997f2a542a28e77
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-06-14 21:03:34 -0300

    Add validateEmail middleware to create user route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
+import { validateEmail } from "./middleware/validateEmail";
 
 const router = Router();
 
 const userController = new UserController();
-router.post("/users", userController.create);
+router.post("/users", validateEmail, userController.create);
 router.get("/users/:id", userController.show);
 
 export { router };
