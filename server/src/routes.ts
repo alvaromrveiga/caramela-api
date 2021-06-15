95afd59880409b75b93b1e2cc9514f202d50573c
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-06-14 21:24:06 -0300

    Add validatePassword middleware to create user route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
 import { validateEmail } from "./middleware/validateEmail";
+import { validatePassword } from "./middleware/validatePassword";
 
 const router = Router();
 
 const userController = new UserController();
-router.post("/users", validateEmail, userController.create);
+router.post("/users", validateEmail, validatePassword, userController.create);
 router.get("/users/:id", userController.show);
 
 export { router };
