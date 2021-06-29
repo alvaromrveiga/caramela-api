f10274b3b676342652e98bfbcecec53ae96fa486
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-06-29 10:03:35 -0300

    Add connection and reset

---
+++ tests/fixtures/database.ts
@@ -0,0 +1,18 @@
+import { Connection, createConnection } from "typeorm";
+import { UsersRepository } from "../../src/controllers/repositories/UsersRepository";
+
+let connection: Connection;
+
+export const resetDatabase = async () => {
+  const allUsers = await UsersRepository.instance.find();
+  await UsersRepository.instance.remove(allUsers);
+};
+
+export const connect = async () => {
+  if (!connection) {
+    connection = await createConnection();
+    await connection.runMigrations();
+  }
+
+  return connection;
+};
