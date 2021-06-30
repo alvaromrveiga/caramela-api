e610e08c85672975e7c797da32b28e21ebeb6e9a
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-06-30 12:31:02 -0300

    Use errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import "reflect-metadata";
 import express from "express";
+import "express-async-errors";
 import createConnection from "./connection";
 import { router } from "./routes";
+import { errorHandler } from "./middleware/errorHandler";
 
 createConnection();
 
 const app = express();
 
 app.use(express.json());
+app.use(errorHandler);
 app.use(router);
 
 export { app };
