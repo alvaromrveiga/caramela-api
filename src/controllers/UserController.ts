6fb50616556cd22164bb985c4130576ae992900d
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-01 11:00:58 -0300

    Return sucess case only
    Remove try catch blocks
    If there is an know error the repository will catch it else the error middleware will

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,36 +3,21 @@ import { UsersRepository } from "./repositories/UsersRepository";
 
 class UserController {
   async create(req: Request, res: Response) {
-    try {
-      const user = await UsersRepository.instance.createAndSave(req.body);
+    const user = await UsersRepository.instance.createAndSave(req.body);
 
-      res.status(201).json(user);
-    } catch (e) {
-      console.error(e);
-      res.status(500).send();
-    }
+    res.status(201).json(user);
   }
 
   async show(req: Request, res: Response) {
-    try {
-      const result = await UsersRepository.instance.showPublic(req.params.id);
+    const user = await UsersRepository.instance.showPublic(req.params.id);
 
-      res.status(result.status).json(result.message);
-    } catch (e) {
-      console.error(e);
-      res.status(500).send();
-    }
+    res.status(200).json(user);
   }
 
   async showSelf(req: Request, res: Response) {
-    try {
-      const result = await UsersRepository.instance.showSelf(req.body.id);
+    const user = await UsersRepository.instance.showSelf(req.body.id);
 
-      res.status(result.status).json(result.message);
-    } catch (e) {
-      console.error(e);
-      res.status(500).send();
-    }
+    res.status(200).json(user);
   }
 }
 
