63605207723c13e282949f6aa6ea4995686b222d
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-03 13:25:17 -0300

    Verify user password on delete

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,20 @@ import { UserController } from "./controllers/UserController";
 import { authenticate } from "./middleware/authentication";
 import { validateEmail } from "./middleware/validateEmail";
 import { validatePassword } from "./middleware/validatePassword";
+import { verifyPassword } from "./middleware/verifyPassword";
 
 const router = Router();
 
 const userController = new UserController();
 router.post("/signup", validateEmail, validatePassword, userController.create);
-router.post("/users/profile", authenticate, userController.deleteUser);
+
+router.post(
+  "/users/profile",
+  authenticate,
+  verifyPassword,
+  userController.deleteUser
+);
+
 router.get("/users/profile", authenticate, userController.showSelf);
 router.get("/users/:id", authenticate, userController.show);
 
