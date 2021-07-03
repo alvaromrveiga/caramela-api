70bb26a0fb7a49f7ea57945caae9478c71dd23b9
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-03 13:16:42 -0300

    Improve readability

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -3,15 +3,11 @@ import bcrypt from "bcrypt";
 const saltRounds = 10;
 
 const hashPasswordAsync = async (password: string) => {
-  return bcrypt.hash(password, saltRounds).then(function (hash) {
-    return hash;
-  });
+  return await bcrypt.hash(password, saltRounds);
 };
 
 const comparePasswordAsync = async (password: string, hash: string) => {
-  return bcrypt.compare(password, hash).then(function (result) {
-    return result;
-  });
+  return await bcrypt.compare(password, hash);
 };
 
 export { hashPasswordAsync, comparePasswordAsync };
