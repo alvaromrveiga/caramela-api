10e88482dc6849276fd4187fa5584c8198fa461f
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-05 14:41:19 -0300

    Add email and password validation

--- src/useCases/User/CreateUserUseCase.ts
+++ src/useCases/User/CreateUserUseCase.ts
@@ -4,13 +4,17 @@ import { generateJwt } from "../../utils/generateJwt";
 import validator from "validator";
 import { ErrorWithStatus } from "../../utils/ErrorWithStatus";
 
+export interface IUserCreateCredentials {
+  name: string;
+  email: string;
+  password: string;
+}
+
 export class CreateUserUseCase {
-  constructor(
-    private body: { name: string; email: string; password: string }
-  ) {}
+  constructor(private body: IUserCreateCredentials) {}
 
   execute = async () => {
-    await this.validateEmail(this.body.email);
+    await this.validateCredentials(this.body);
 
     const user = UsersRepository.instance.create(this.body);
 
@@ -23,7 +27,12 @@ export class CreateUserUseCase {
     return UsersRepository.instance.getUserCredentials(user);
   };
 
-  validateEmail = async (email: string) => {
+  private validateCredentials = async (body: IUserCreateCredentials) => {
+    await this.validateEmail(body.email);
+    await this.validatePassword(body.password);
+  };
+
+  private validateEmail = async (email: string) => {
     if (validator.isEmail(email)) {
       if (await UsersRepository.instance.findOne({ email })) {
         throw new ErrorWithStatus(400, "Email already in use!");
@@ -32,5 +41,17 @@ export class CreateUserUseCase {
     }
 
     throw new ErrorWithStatus(400, "Invalid email");
+  };
+
+  private validatePassword = async (password: string) => {
+    const minLength = 8;
+
+    if (password.length < minLength) {
+      throw new ErrorWithStatus(
+        400,
+        `Password shorter than ${minLength} characters`
+      );
+    }
+    return true;
   };
 }
