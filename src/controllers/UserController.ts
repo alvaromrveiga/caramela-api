46f114caaa68ccc5429076ae3fc66c30542e4b70
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-05 12:13:59 -0300

    Standardize using arrow functions

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,29 +2,29 @@ import { Request, Response } from "express";
 import { UsersRepository } from "./repositories/UsersRepository";
 
 class UserController {
-  async create(req: Request, res: Response) {
+  create = async (req: Request, res: Response) => {
     const user = await UsersRepository.instance.createAndSave(req.body);
 
     res.status(201).json(user);
-  }
+  };
 
-  async show(req: Request, res: Response) {
+  show = async (req: Request, res: Response) => {
     const user = await UsersRepository.instance.showPublic(req.params.id);
 
     res.status(200).json(user);
-  }
+  };
 
-  async showSelf(req: Request, res: Response) {
+  showSelf = async (req: Request, res: Response) => {
     const user = await UsersRepository.instance.showSelf(req.body.id);
 
     res.status(200).json(user);
-  }
+  };
 
-  async deleteUser(req: Request, res: Response) {
+  deleteUser = async (req: Request, res: Response) => {
     await UsersRepository.instance.deleteUser(req.body.id);
 
     res.status(200).json({ message: "User removed successfully!" });
-  }
+  };
 }
 
 export { UserController };
