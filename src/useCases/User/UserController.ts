ec63cc5c86a818a067c38999406c9b47fd371cb2
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-05 16:34:12 -0300

    Create login route controller

--- src/useCases/User/UserController.ts
+++ src/useCases/User/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 import { DeleteUserUseCase } from "./DeleteUserUseCase";
+import { LoginUserUseCase } from "./LoginUserUseCase";
 import { ShowUserUseCase } from "./ShowUserUseCase";
 
 class UserController {
@@ -8,6 +9,15 @@ class UserController {
     const user = await new CreateUserUseCase(req.body).execute();
 
     res.status(201).json(user);
+  };
+
+  login = async (req: Request, res: Response) => {
+    const user = await new LoginUserUseCase(
+      req.body.email,
+      req.body.password
+    ).execute();
+
+    res.status(200).json(user);
   };
 
   show = async (req: Request, res: Response) => {
