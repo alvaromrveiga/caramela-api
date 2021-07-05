178f19cb19ef982e26311f6f542998b72d8f3307
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-05 13:57:44 -0300

    Remove delete

--- src/repositories/UsersRepository.ts
+++ src/repositories/UsersRepository.ts
@@ -1,22 +1,11 @@
 import { EntityRepository, getCustomRepository, Repository } from "typeorm";
 import { User } from "../models/User";
-import { ErrorWithStatus } from "../utils/ErrorWithStatus";
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   static get instance(): UsersRepository {
     return getCustomRepository(this);
   }
-
-  deleteUser = async (id: string) => {
-    const user = await this.delete({ id });
-
-    if (!user) {
-      throw new ErrorWithStatus(401, "Please authenticate");
-    }
-
-    return true;
-  };
 
   getUserCredentials = (user: User) => {
     return {
