1d3cd13f5e0263e74e8df4727b91f1bd4e53aa9f
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-06 09:04:12 -0300

    Do not acess DB on showSelf

--- src/useCases/User/ShowUserUseCase.ts
+++ src/useCases/User/ShowUserUseCase.ts
@@ -1,8 +1,9 @@
+import { User } from "../../models/User";
 import { UsersRepository } from "../../repositories/UsersRepository";
 import { ErrorWithStatus } from "../../utils/ErrorWithStatus";
 
 export class ShowUserUseCase {
-  constructor(private id: string) {}
+  constructor(private id?: string) {}
 
   private execute = async () => {
     const user = await UsersRepository.instance.findOne(this.id);
@@ -20,9 +21,7 @@ export class ShowUserUseCase {
     return UsersRepository.instance.getPublicUserCredentials(user);
   };
 
-  self = async () => {
-    const user = await this.execute();
-
+  self = async (user: User) => {
     return UsersRepository.instance.getUserCredentials(user);
   };
 }
