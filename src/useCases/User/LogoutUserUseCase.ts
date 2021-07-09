3082997ef02924c3cd59b412177bb117fee7c6e5
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-09 09:30:11 -0300

    Init file

---
+++ src/useCases/User/LogoutUserUseCase.ts
@@ -0,0 +1,16 @@
+import { User } from "../../models/User";
+import { UsersRepository } from "../../repositories/UsersRepository";
+
+export class LogoutUserUseCase {
+  constructor(private user: User, private loginToken: string) {}
+
+  execute = async () => {
+    this.user.tokens = this.user.tokens.filter((token) => {
+      return this.loginToken !== token;
+    });
+
+    await UsersRepository.instance.save(this.user);
+
+    return true;
+  };
+}
