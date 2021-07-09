42a9f644c72babaee1f9742db148b7388820967d
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-09 09:47:52 -0300

    Add logOutAll route and rename methods

--- src/routes.ts
+++ src/routes.ts
@@ -6,8 +6,9 @@ const router = Router();
 const userController = new UserController();
 
 router.post("/signup", userController.create);
-router.post("/login", userController.login);
-router.post("/users/logout", ensureAuthenticated, userController.logout);
+router.post("/login", userController.logIn);
+router.post("/users/logout", ensureAuthenticated, userController.logOut);
+router.post("/users/logout-all", ensureAuthenticated, userController.logOutAll);
 router.post("/users/profile", ensureAuthenticated, userController.deleteUser);
 router.get("/users/profile", ensureAuthenticated, userController.showSelf);
 router.get("/users/:id", ensureAuthenticated, userController.show);
