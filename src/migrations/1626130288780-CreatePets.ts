04d7c868b6cbfdc2a381b5d5c60f8f32f8b2942a
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-12 20:18:55 -0300

    Create pets table

---
+++ src/migrations/1626130288780-CreatePets.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreatePets1626130288780 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "pets",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "gender",
+            type: "varchar",
+            isNullable: true,
+          },
+          {
+            name: "weight",
+            type: "decimal",
+            isNullable: true,
+          },
+          {
+            name: "birthday",
+            type: "timestamp",
+            isNullable: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKUser",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("pets");
+  }
+}
