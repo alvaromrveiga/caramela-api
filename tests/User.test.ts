7726703be83ab0d6fa584ed6a1d1f725b8456aa9
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-13 12:24:21 -0300

    Revert "Test create user on separete file"
    This reverts commit 4c3182d25ef1652467d8d18a759f9dc82b43aa28.

--- tests/User.test.ts
+++ tests/User.test.ts
@@ -37,122 +37,122 @@ describe("Users", () => {
     ({ userOne, userTwo } = await getUsers());
   });
 
-  // describe("Create", () => {
-  //   it("Should create new user", async () => {
-  //     await request(app)
-  //       .post("/signup")
-  //       .send({
-  //         name: "notEqualUserOne",
-  //         email: "notEqualUserOne@test.com",
-  //         password: "tester-pass",
-  //       })
-  //       .expect(201);
+  describe("Create", () => {
+    it("Should create new user", async () => {
+      await request(app)
+        .post("/signup")
+        .send({
+          name: "notEqualUserOne",
+          email: "notEqualUserOne@test.com",
+          password: "tester-pass",
+        })
+        .expect(201);
 
-  //     const user =
-  //       (await UsersRepository.instance.findOne({
-  //         email: "notEqualUserOne@test.com",
-  //       })) || new User();
+      const user =
+        (await UsersRepository.instance.findOne({
+          email: "notEqualUserOne@test.com",
+        })) || new User();
 
-  //     expect(user.id).toMatch(UUID_RegExp.UUID_RegExp);
+      expect(user.id).toMatch(UUID_RegExp.UUID_RegExp);
 
-  //     expect(user).toMatchObject({
-  //       name: "notEqualUserOne",
-  //       email: "notEqualUserOne@test.com",
-  //     });
-  //     expect(user.password).not.toEqual("tester-pass");
-  //     expect(user).toHaveProperty("created_at");
-  //   });
+      expect(user).toMatchObject({
+        name: "notEqualUserOne",
+        email: "notEqualUserOne@test.com",
+      });
+      expect(user.password).not.toEqual("tester-pass");
+      expect(user).toHaveProperty("created_at");
+    });
 
-  //   it("Should not create user with email already in use", async () => {
-  //     await request(app)
-  //       .post("/signup")
-  //       .send({
-  //         email: userOne.email,
-  //         password: "DoesNotMatter",
-  //       })
-  //       .expect(400);
+    it("Should not create user with email already in use", async () => {
+      await request(app)
+        .post("/signup")
+        .send({
+          email: userOne.email,
+          password: "DoesNotMatter",
+        })
+        .expect(400);
 
-  //     const usersWithEmail = await UsersRepository.instance.find({
-  //       email: userOne.email,
-  //     });
+      const usersWithEmail = await UsersRepository.instance.find({
+        email: userOne.email,
+      });
 
-  //     expect(usersWithEmail.length).toEqual(1);
-  //   });
+      expect(usersWithEmail.length).toEqual(1);
+    });
 
-  //   it("Should not create user with invalid password", async () => {
-  //     await request(app)
-  //       .post("/signup")
-  //       .send({
-  //         email: "notUserOneEmail@test.com",
-  //         password: "123",
-  //       })
-  //       .expect(400);
-  //   });
+    it("Should not create user with invalid password", async () => {
+      await request(app)
+        .post("/signup")
+        .send({
+          email: "notUserOneEmail@test.com",
+          password: "123",
+        })
+        .expect(400);
+    });
 
-  //   it("Should not save password as plain text", async () => {
-  //     await request(app)
-  //       .post("/signup")
-  //       .send({
-  //         email: "noUser@test.com",
-  //         password: "shouldNotBePlainText",
-  //       })
-  //       .expect(201);
+    it("Should not save password as plain text", async () => {
+      await request(app)
+        .post("/signup")
+        .send({
+          email: "noUser@test.com",
+          password: "shouldNotBePlainText",
+        })
+        .expect(201);
 
-  //     const user = await UsersRepository.instance.findOne({
-  //       email: "noUser@test.com",
-  //     });
+      const user = await UsersRepository.instance.findOne({
+        email: "noUser@test.com",
+      });
 
-  //     expect(user?.password).not.toEqual("shouldNotBePlainText");
-  //   });
+      expect(user?.password).not.toEqual("shouldNotBePlainText");
+    });
 
-  //   it("Should not response user password", async () => {
-  //     const response = await request(app)
-  //       .post("/signup")
-  //       .send({
-  //         name: "Tester",
-  //         email: "test@test.com",
-  //         password: "StroNg_@Pa$$woRd:)",
-  //       })
-  //       .expect(201);
+    it("Should not response user password", async () => {
+      const response = await request(app)
+        .post("/signup")
+        .send({
+          name: "Tester",
+          email: "test@test.com",
+          password: "StroNg_@Pa$$woRd:)",
+        })
+        .expect(201);
 
-  //     expect(Object.keys(response.body)).not.toContain("password");
-  //   });
+      expect(Object.keys(response.body)).not.toContain("password");
+    });
 
-  //   it("Should contain id information inside JsonWebToken", async () => {
-  //     expect(process.env.JWT_SECRET).not.toBeUndefined();
+    it("Should contain id information inside JsonWebToken", async () => {
+      expect(process.env.JWT_SECRET).not.toBeUndefined();
 
-  //     if (!process.env.JWT_SECRET) {
-  //       throw new Error("No JWT_TOKEN on .env file");
-  //     }
+      if (!process.env.JWT_SECRET) {
+        throw new Error("No JWT_TOKEN on .env file");
+      }
 
-  //     const token = userOne.tokens[0];
-  //     const decoded = jwt.verify(token, process.env.JWT_SECRET) as {
-  //       id: string;
-  //     };
+      const token = userOne.tokens[0];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET) as {
+        id: string;
+      };
 
-  //     expect(decoded.id).toEqual(userOne.id);
-  //   });
+      expect(decoded.id).toEqual(userOne.id);
+    });
 
-  //   it("Should show error message if no email field", async () => {
-  //     await request(app)
-  //       .post("/signup")
-  //       .send({
-  //         name: userOne.name,
-  //         password: rawUserOne.password,
-  //       })
-  //       .expect(400);
-  //   });
+    it("Should show error message if no email field", async () => {
+      await request(app)
+        .post("/signup")
+        .send({
+          name: userOne.name,
+          password: rawUserOne.password,
+        })
+        .expect(400);
+    });
 
-  //   it("Should show error message if no password field", async () => {
-  //     await request(app)
-  //       .post("/signup")
-  //       .send({
-  //         name: userOne.name,
-  //         email: userOne.email,
-  //       })
-  //       .expect(400);
-  //   });
-  // });
+    it("Should show error message if no password field", async () => {
+      await request(app)
+        .post("/signup")
+        .send({
+          name: userOne.name,
+          email: userOne.email,
+        })
+        .expect(400);
+    });
+  });
 
   describe("Login", () => {
     it("Should login user", async () => {
