85b50f732992af37539aa6c4895b0662d769126b
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-07-14 13:28:37 -0300

    Add create pet route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 import { UserController } from "./useCases/User/UserController";
+import { PetController } from "./useCases/Pet/PetController";
 
 const router = Router();
 const userController = new UserController();
+const petController = new PetController();
 
 router.post("/signup", userController.create);
 router.post("/login", userController.logIn);
@@ -13,5 +15,7 @@ router.get("/users/profile", ensureAuthenticated, userController.showSelf);
 router.get("/users/:id", ensureAuthenticated, userController.show);
 router.put("/users/profile", ensureAuthenticated, userController.update);
 router.delete("/users/profile", ensureAuthenticated, userController.deleteUser);
+
+router.post("/users/pets", ensureAuthenticated, petController.create);
 
 export { router };
---
+++ src/useCases/Pet/PetController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { CreatePetUseCase } from "./CreatePetUseCase";
+
+export class PetController {
+  create = async (req: Request, res: Response) => {
+    const pet = await new CreatePetUseCase(
+      res.locals.user.id,
+      req.body
+    ).execute();
+
+    res.status(201).json(pet);
+  };
+}
