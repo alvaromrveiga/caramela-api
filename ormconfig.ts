56eb74b731fd67d5dc4b99747c76031d7f6ce749
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-08-20 12:50:17 -0300

    Use came credentials as docker-compose
    Since we are now using docker on dev, those credentials are not sensitive and do not need to be on .env

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,11 +1,10 @@
 module.exports = {
   type: "postgres",
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  logging: process.env.TYPEORM_LOGGING === "true",
+  host: "localhost",
+  port: "5432",
+  username: "postgres",
+  password: "docker",
+  database: "caramela",
   migrations: ["./src/migrations/**.ts"],
   entities: ["./src/models/**.ts"],
   cli: {
