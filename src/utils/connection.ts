dea1d562136ead4c716d655dec8698281ed146c4
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-08-20 13:17:01 -0300

    Use different database for tests

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -2,11 +2,14 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
-
+  
   return createConnection(
     Object.assign(defaultOptions, {
-      host: "database-caramela",
-      database: defaultOptions.database,
+      host: process.env.NODE_ENV === "test" ? "localhost" : "database-caramela",
+      database:
+        process.env.NODE_ENV === "test"
+          ? "database-caramela_test"
+          : defaultOptions.database,
     })
   );
 };
