509cfa9da0895036a97a51cefe24f33056106163
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-08-20 12:51:56 -0300

    Use docker database as host
    Cannot connect using default localhost

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,3 +1,12 @@
-import { createConnection } from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default createConnection;
+export default async (): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: "database-caramela",
+      database: defaultOptions.database,
+    })
+  );
+};
