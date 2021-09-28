4cb84b3df343c8b1dc746d9ae27dcf457e0086b9
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-09-28 17:41:02 -0300

    Use new delete user controller

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateUserController } from "../../../../modules/users/useCases/createUser/CreateUserController";
+import { DeleteUserController } from "../../../../modules/users/useCases/deleteUser/DeleteUserController";
 import { LoginUserController } from "../../../../modules/users/useCases/loginUser/LoginUserController";
 import { UserController } from "../../../../modules/users/useCases/UserController";
 import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
@@ -16,7 +17,12 @@ router.post("/users/logout-all", ensureAuthenticated, userController.logOutAll);
 router.get("/users/profile", ensureAuthenticated, userController.showSelf);
 router.get("/users/:id", ensureAuthenticated, userController.show);
 router.put("/users/profile", ensureAuthenticated, userController.update);
-router.delete("/users/profile", ensureAuthenticated, userController.deleteUser);
+
+router.delete(
+  "/users/profile",
+  ensureAuthenticated,
+  new DeleteUserController().handle
+);
 
 router.use(petsRoutes);
 
