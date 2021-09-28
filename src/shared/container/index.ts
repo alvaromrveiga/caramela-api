fb014117fb4857b3bd5bad2de5f162db8cc4d7ce
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-09-28 15:51:24 -0300

    Register users repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,5 +2,12 @@ import { container } from "tsyringe";
 
 import { PetsRepository } from "../../modules/pets/infra/typeorm/repositories/PetsRepository";
 import { IPetsRepository } from "../../modules/pets/repositories/IPetsRepository";
+import { UsersRepositoryNew } from "../../modules/users/infra/typeorm/repositories/UsersRepositoryNew";
+import { IUsersRepository } from "../../modules/users/repositories/IUsersRepository";
 
 container.registerSingleton<IPetsRepository>("PetsRepository", PetsRepository);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepositoryNew
+);
