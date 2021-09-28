de3618b46ec5571f8744f34b7ed5639245b48569
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-09-28 17:35:33 -0300

    Init file

---
+++ src/modules/users/useCases/loginUser/LoginUserUseCase.spec.ts
@@ -0,0 +1,45 @@
+import { ErrorWithStatus } from "../../../../utils/ErrorWithStatus";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { LoginUserUseCase } from "./LoginUserUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let loginUserUseCase: LoginUserUseCase;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Delete User use case", () => {
+  beforeEach(async () => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    loginUserUseCase = new LoginUserUseCase(inMemoryUsersRepository);
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+
+    await createUserUseCase.execute({
+      name: "Tester",
+      email: "tester@mail.com",
+      password: "testerPa$$w0rd",
+    });
+  });
+
+  it("Should login user", async () => {
+    const user = await loginUserUseCase.execute(
+      "tester@mail.com",
+      "testerPa$$w0rd"
+    );
+
+    expect(user.name).toEqual("Tester");
+    expect(user.email).toEqual("tester@mail.com");
+    expect(user.tokens.length).toEqual(1);
+  });
+
+  it("Should not login user if email is wrong", async () => {
+    await expect(
+      loginUserUseCase.execute("wrongEmail", "testerPa$$w0rd")
+    ).rejects.toEqual(new ErrorWithStatus(400, "Invalid email or password!"));
+  });
+
+  it("Should not login user if password is wrong", async () => {
+    await expect(
+      loginUserUseCase.execute("tester@mail.com", "wrongPassword")
+    ).rejects.toEqual(new ErrorWithStatus(400, "Invalid email or password!"));
+  });
+});
