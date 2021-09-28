8e0c2648924ab13cd58218c4789d96a2df49bf4f
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-09-28 14:45:10 -0300

    Init file

---
+++ src/modules/users/repositories/in-memory/InMemoryUsersRepository.ts
@@ -0,0 +1,28 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../infra/typeorm/entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  constructor(private users: User[]) {
+    this.users = [];
+  }
+
+  async createAndSave(data: ICreateUserDTO): Promise<void> {
+    const user = new User();
+    Object.assign(user, data);
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    return this.users.find((user) => {
+      return user.email === email;
+    });
+  }
+
+  async findById(id: string): Promise<User | undefined> {
+    return this.users.find((user) => {
+      return user.id === id;
+    });
+  }
+}
