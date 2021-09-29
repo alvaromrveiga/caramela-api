44f44c0614aeb4cc8ec6f6a93cfd62ad81790a2b
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-09-29 10:24:00 -0300

    Use new logoutAllUser controller

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateUserController } from "../../../../modules/users/useCases/createUser/CreateUserController";
 import { DeleteUserController } from "../../../../modules/users/useCases/deleteUser/DeleteUserController";
 import { LoginUserController } from "../../../../modules/users/useCases/loginUser/LoginUserController";
+import { LogoutAllUserController } from "../../../../modules/users/useCases/logoutAllUser/LogoutAllUserController";
 import { LogoutUserController } from "../../../../modules/users/useCases/logoutUser/LogoutUserController";
 import { UserController } from "../../../../modules/users/useCases/UserController";
 import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
@@ -20,7 +21,11 @@ router.post(
   new LogoutUserController().handle
 );
 
-router.post("/users/logout-all", ensureAuthenticated, userController.logOutAll);
+router.post(
+  "/users/logout-all",
+  ensureAuthenticated,
+  new LogoutAllUserController().handle
+);
 router.get("/users/profile", ensureAuthenticated, userController.showSelf);
 router.get("/users/:id", ensureAuthenticated, userController.show);
 router.put("/users/profile", ensureAuthenticated, userController.update);
