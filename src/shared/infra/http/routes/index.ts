316bbda5062ddba65cd9d5acd2234f67b686698c
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-09-29 11:37:13 -0300

    Use new show private user controller

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -5,6 +5,7 @@ import { DeleteUserController } from "../../../../modules/users/useCases/deleteU
 import { LoginUserController } from "../../../../modules/users/useCases/loginUser/LoginUserController";
 import { LogoutAllUserController } from "../../../../modules/users/useCases/logoutAllUser/LogoutAllUserController";
 import { LogoutUserController } from "../../../../modules/users/useCases/logoutUser/LogoutUserController";
+import { ShowPrivateUserController } from "../../../../modules/users/useCases/showPrivateUser/ShowPrivateUserController";
 import { ShowPublicUserController } from "../../../../modules/users/useCases/showPublicUser/ShowPublicUserController";
 import { UserController } from "../../../../modules/users/useCases/UserController";
 import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
@@ -28,7 +29,11 @@ router.post(
   new LogoutAllUserController().handle
 );
 
-router.get("/users/profile", ensureAuthenticated, userController.showSelf);
+router.get(
+  "/users/profile",
+  ensureAuthenticated,
+  new ShowPrivateUserController().handle
+);
 
 router.get(
   "/users/:id",
