0fde79350d6d9df4dc18dd154207bfa3aad478fe
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-06 17:42:00 -0300

    Create pet

---
+++ src/modules/pets/useCases/createPet/CreatePetUseCase.spec.ts
@@ -0,0 +1,102 @@
+import { validate } from "uuid";
+
+import { ErrorWithStatus } from "../../../../utils/ErrorWithStatus";
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { InMemoryPetsRepository } from "../../repositories/in-memory/InMemoryPetsRepository";
+import { CreatePetUseCase } from "./CreatePetUseCase";
+
+let inMemoryPetsRepository: InMemoryPetsRepository;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let createPetUseCase: CreatePetUseCase;
+
+let userId: string;
+
+describe("Create Pet use case", () => {
+  beforeEach(async () => {
+    inMemoryPetsRepository = new InMemoryPetsRepository();
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+
+    createPetUseCase = new CreatePetUseCase(
+      inMemoryPetsRepository,
+      inMemoryUsersRepository
+    );
+
+    await inMemoryUsersRepository.createAndSave({
+      email: "tester@mail.com",
+      name: "Tester",
+      password: "testerPassword",
+    });
+
+    const user = await inMemoryUsersRepository.findByEmail("tester@mail.com");
+
+    expect(user).toBeDefined();
+    if (user) {
+      userId = user.id;
+    }
+  });
+
+  it("Should create pet", async () => {
+    await createPetUseCase.execute({
+      name: "Petster",
+      user_id: userId,
+      species: "Cat",
+      birthday: new Date(),
+      gender: "Female",
+      weight_kg: 4.5,
+    });
+
+    const pet = await inMemoryPetsRepository.findByUserIDAndName(
+      userId,
+      "Petster"
+    );
+
+    expect(pet).toBeDefined();
+
+    if (pet) {
+      expect(pet.name).toEqual("Petster");
+      expect(validate(pet.id)).toEqual(true);
+      expect(validate(pet.user_id)).toEqual(true);
+      expect(pet.species).toEqual("Cat");
+      expect(pet.gender).toEqual("Female");
+      expect(pet.weight_kg).toEqual(4.5);
+      expect(pet.birthday).toBeInstanceOf(Date);
+    }
+  });
+
+  it("Should not create pet with invalid user", async () => {
+    await expect(
+      createPetUseCase.execute({
+        name: "Petster",
+        user_id: "6cd3cd38-0aca-4e13-a697-cc4c0c2d7a06",
+        species: "Cat",
+        birthday: new Date(),
+        gender: "Female",
+        weight_kg: 4.5,
+      })
+    ).rejects.toEqual(new ErrorWithStatus(401, "Please authenticate"));
+  });
+
+  it("Should not create pet with name already in use for the same user", async () => {
+    await createPetUseCase.execute({
+      name: "Petster",
+      user_id: userId,
+      species: "Cat",
+      birthday: new Date(),
+      gender: "Female",
+      weight_kg: 4.5,
+    });
+
+    await expect(
+      createPetUseCase.execute({
+        name: "Petster",
+        user_id: userId,
+        species: "Dog",
+        birthday: new Date(),
+        gender: "Male",
+        weight_kg: 11.3,
+      })
+    ).rejects.toEqual(
+      new ErrorWithStatus(400, "You already have a pet with that name!")
+    );
+  });
+});
--- src/modules/pets/useCases/createPet/CreatePetUseCase.ts
+++ src/modules/pets/useCases/createPet/CreatePetUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { ErrorWithStatus } from "../../../../utils/ErrorWithStatus";
+import { IUsersRepository } from "../../../users/repositories/IUsersRepository";
 import { ICreatePetDTO } from "../../dtos/ICreatePetDTO";
 import { IPetsRepository } from "../../repositories/IPetsRepository";
 
@@ -8,18 +9,52 @@ import { IPetsRepository } from "../../repositories/IPetsRepository";
 export class CreatePetUseCase {
   constructor(
     @inject("PetsRepository")
-    private petsRepository: IPetsRepository
+    private petsRepository: IPetsRepository,
+
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
   ) {}
 
-  execute = async (data: ICreatePetDTO): Promise<void> => {
+  async execute(data: ICreatePetDTO): Promise<void> {
+    await this.validateCredentials(data);
+
+    await this.petsRepository.createAndSave(data);
+  }
+
+  private async validateCredentials(data: ICreatePetDTO): Promise<void> {
+    await this.validateUser(data.user_id);
+
+    await this.validatePetName(data);
+
+    this.validateSpecies(data.species);
+  }
+
+  private async validateUser(userId: string): Promise<void> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      throw new ErrorWithStatus(401, "Please authenticate");
+    }
+  }
+
+  private async validatePetName(data: ICreatePetDTO): Promise<void> {
     if (!data.name) {
       throw new ErrorWithStatus(400, "Invalid pet name");
     }
 
-    if (!data.species) {
+    const petAlreadyExists = await this.petsRepository.findByUserIDAndName(
+      data.user_id,
+      data.name
+    );
+
+    if (petAlreadyExists) {
+      throw new ErrorWithStatus(400, "You already have a pet with that name!");
+    }
+  }
+
+  private validateSpecies(species: string): void {
+    if (!species) {
       throw new ErrorWithStatus(400, "Please choose your pet species!");
     }
-
-    await this.petsRepository.createAndSave(data);
-  };
+  }
 }
