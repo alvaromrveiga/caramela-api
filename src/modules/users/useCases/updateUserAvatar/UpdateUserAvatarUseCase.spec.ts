cade5c149febecc7a1a0023637a723df24e855db
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-13 20:35:33 -0300

    Update user avatar use case

---
+++ src/modules/users/useCases/updateUserAvatar/UpdateUserAvatarUseCase.spec.ts
@@ -0,0 +1,68 @@
+import { mock } from "jest-mock-extended";
+
+import { IStorageProvider } from "../../../../shared/container/providers/StorageProvider/IStorageProvider";
+import { ErrorWithStatus } from "../../../../utils/ErrorWithStatus";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let storageProviderMock: IStorageProvider;
+let updateUserAvatarUseCase: UpdateUserAvatarUseCase;
+
+describe("Update User Avatar use case", () => {
+  beforeEach(async () => {
+    storageProviderMock = mock<IStorageProvider>();
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+
+    updateUserAvatarUseCase = new UpdateUserAvatarUseCase(
+      inMemoryUsersRepository,
+      storageProviderMock
+    );
+
+    await inMemoryUsersRepository.createAndSave({
+      name: "Tester",
+      email: "tester@mail.com",
+      password: "testerPa$$w0rd",
+    });
+  });
+
+  it("Should update user avatar", async () => {
+    let user = await inMemoryUsersRepository.findByEmail("tester@mail.com");
+
+    expect(user).toBeDefined();
+    if (user) {
+      await updateUserAvatarUseCase.execute(user.id, "testFile.png");
+
+      expect(storageProviderMock.save).toHaveBeenCalledWith(
+        "testFile.png",
+        "usersAvatars"
+      );
+
+      expect(user.avatar).toEqual("testFile.png");
+
+      await updateUserAvatarUseCase.execute(user.id, "testFile2.png");
+
+      expect(storageProviderMock.delete).toHaveBeenCalledWith(
+        "testFile.png",
+        "usersAvatars"
+      );
+
+      expect(storageProviderMock.save).toHaveBeenCalledWith(
+        "testFile2.png",
+        "usersAvatars"
+      );
+
+      user = await inMemoryUsersRepository.findByEmail("tester@mail.com");
+      expect(user).toBeDefined();
+      if (user) {
+        expect(user.avatar).toEqual("testFile2.png");
+      }
+    }
+  });
+
+  it("Should not update invalid user's avatar", async () => {
+    await expect(
+      updateUserAvatarUseCase.execute("invalidUser", "avatarFile.png")
+    ).rejects.toEqual(new ErrorWithStatus(401, "Please authenticate"));
+  });
+});
---
+++ src/modules/users/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,41 @@
+import { inject, injectable } from "tsyringe";
+
+import { IStorageProvider } from "../../../../shared/container/providers/StorageProvider/IStorageProvider";
+import { ErrorWithStatus } from "../../../../utils/ErrorWithStatus";
+import { User } from "../../infra/typeorm/entities/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+export class UpdateUserAvatarUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
+  ) {}
+
+  async execute(userId: string, avatarFile: string): Promise<void> {
+    const user = await this.getUser(userId);
+
+    if (user.avatar) {
+      this.storageProvider.delete(user.avatar, "usersAvatars");
+    }
+
+    await this.storageProvider.save(avatarFile, "usersAvatars");
+
+    user.avatar = avatarFile;
+
+    await this.usersRepository.createAndSave(user);
+  }
+
+  private async getUser(userId: string): Promise<User> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      throw new ErrorWithStatus(401, "Please authenticate");
+    }
+
+    return user;
+  }
+}
