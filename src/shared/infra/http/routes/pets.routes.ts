4927e93f6ab2c35153bf88e4d81f8a79281b14ed
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-14 11:13:25 -0300

    Add update pet avatar route

--- src/shared/infra/http/routes/pets.routes.ts
+++ src/shared/infra/http/routes/pets.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { CreatePetController } from "../../../../modules/pets/useCases/createPet/CreatePetController";
 import { DeletePetController } from "../../../../modules/pets/useCases/deletePet/DeletePetController";
 import { ShowAllPetsController } from "../../../../modules/pets/useCases/showAllPets/ShowAllPetsController";
 import { ShowPetController } from "../../../../modules/pets/useCases/showPet/ShowPetController";
 import { UpdatePetController } from "../../../../modules/pets/useCases/updatePet/UpdatePetController";
+import { UpdatePetAvatarController } from "../../../../modules/pets/useCases/updatePetAvatar/UpdatePetAvatarController";
+import uploadConfig from "../../../../utils/upload";
 import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
 
 const petsRoutes = Router();
+
+const uploadAvatar = multer(uploadConfig);
 
 petsRoutes.post(
   "/users/pets",
@@ -37,6 +42,13 @@ petsRoutes.put(
   "/users/pets/:pet",
   ensureAuthenticated,
   new UpdatePetController().handle
+);
+
+petsRoutes.patch(
+  "/users/pets/:pet/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  new UpdatePetAvatarController().handle
 );
 
 export { petsRoutes };
