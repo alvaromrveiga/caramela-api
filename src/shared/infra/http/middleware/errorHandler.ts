4060f1ccbfc7a8351e482cecfca32e15d8a18abb
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-18 23:41:19 -0300

    Catch multer file upload errors

--- src/shared/infra/http/middleware/errorHandler.ts
+++ src/shared/infra/http/middleware/errorHandler.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { MulterError } from "multer";
 
 import { ErrorWithStatus } from "../../../../utils/ErrorWithStatus";
 
@@ -7,9 +8,16 @@ export function errorHandler(
   request: Request,
   response: Response,
   next: NextFunction
-) {
+): Response {
   if (error instanceof ErrorWithStatus) {
     return response.status(error.status).json({ error: error.message });
   }
+
+  if (error instanceof MulterError) {
+    return response
+      .status(400)
+      .json({ error: `File upload error: ${error.message}` });
+  }
+
   return response.status(500).json({ error: error.message });
 }
