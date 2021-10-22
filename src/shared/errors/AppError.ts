fa74cd2925e49f472a0b05ff32a8f36763877b07
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-22 16:23:36 -0300

    Extend Error built-in class

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,9 +1,14 @@
-export class AppError {
+export class AppError extends Error {
   readonly message: string;
   readonly statusCode: number;
 
   constructor(message: string, statusCode = 400) {
+    super(message);
+
     this.message = message;
     this.statusCode = statusCode;
+
+    // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, AppError.prototype);
   }
 }
