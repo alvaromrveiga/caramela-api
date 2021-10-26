bb2aacd83fd1fb42f620553a81bc4f0db9d4bd8c
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-26 10:41:48 -0300

    Export tokenSecret

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,4 +1,12 @@
+import { NoJwtSecretError } from "../shared/infra/http/middleware/errors/NoJwtSecretError";
+
+if (!process.env.JWT_SECRET) {
+  throw new NoJwtSecretError();
+}
+
 const tokenExpiresIn = "15m";
+const tokenSecret = process.env.JWT_SECRET;
+
 const refreshTokenExpiresIn = "30d";
 
-export { tokenExpiresIn, refreshTokenExpiresIn };
+export { tokenExpiresIn, tokenSecret, refreshTokenExpiresIn };
