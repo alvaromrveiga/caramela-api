2bb354648d37b06bebba1334d29d8b310c4ee25c
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-27 15:34:19 -0300

    Add refresh token secret

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,12 +1,23 @@
-import { NoJwtSecretError } from "../shared/infra/http/middleware/errors/NoJwtSecretError";
+import { NoJwtSecretError } from "./errors/NoJwtSecretError";
+import { NoRefreshTokenSecretError } from "./errors/NoRefreshTokenSecretError";
 
 if (!process.env.JWT_SECRET) {
   throw new NoJwtSecretError();
+}
+
+if (!process.env.REFRESH_TOKEN_SECRET) {
+  throw new NoRefreshTokenSecretError();
 }
 
 const tokenExpiresIn = "15m";
 const tokenSecret = process.env.JWT_SECRET;
 
 const refreshTokenExpiresIn = "30d";
+const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
 
-export { tokenExpiresIn, tokenSecret, refreshTokenExpiresIn };
+export {
+  tokenExpiresIn,
+  tokenSecret,
+  refreshTokenExpiresIn,
+  refreshTokenSecret,
+};
