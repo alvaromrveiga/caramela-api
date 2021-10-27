bfafb44b693bbc1153b638e6f771787636353be5
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-27 16:54:00 -0300

    Add usersTokensRepository dependency

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,11 +5,18 @@ import "./providers/StorageProvider";
 import { PetsRepository } from "../../modules/pets/infra/typeorm/repositories/PetsRepository";
 import { IPetsRepository } from "../../modules/pets/repositories/IPetsRepository";
 import { UsersRepository } from "../../modules/users/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "../../modules/users/infra/typeorm/repositories/UsersTokensRepository";
 import { IUsersRepository } from "../../modules/users/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "../../modules/users/repositories/IUsersTokensRepository";
 
 container.registerSingleton<IPetsRepository>("PetsRepository", PetsRepository);
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+  "UsersTokensRepository",
+  UsersTokensRepository
 );
