7ae7d3f756a8bc138b7dccddac900c77bc505b25
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-27 16:54:12 -0300

    Init file

---
+++ src/modules/users/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,50 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUserTokenDTO } from "../../../dtos/ICreateUserTokenDTO";
+import { IUsersTokensRepository } from "../../../repositories/IUsersTokensRepository";
+import { UserTokens } from "../entities/UserTokens";
+
+export class UsersTokensRepository implements IUsersTokensRepository {
+  private repository: Repository<UserTokens>;
+
+  constructor() {
+    this.repository = getRepository(UserTokens);
+  }
+
+  async createAndSave(data: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = this.repository.create(data);
+
+    await this.repository.save(userToken);
+
+    return userToken;
+  }
+
+  async findByUserAndRefreshToken(
+    userId: string,
+    refreshToken: string
+  ): Promise<UserTokens | undefined> {
+    return this.repository.findOne({
+      where: [
+        {
+          user_id: userId,
+          refresh_token: refreshToken,
+        },
+      ],
+    });
+  }
+
+  async findByRefreshToken(
+    refreshToken: string
+  ): Promise<UserTokens | undefined> {
+    return this.repository.findOne({ refresh_token: refreshToken });
+  }
+
+  async deleteById(tokenId: string): Promise<void> {
+    await this.repository.delete({ id: tokenId });
+  }
+
+  async deleteAllByUserId(userId: string): Promise<void> {
+    const userTokens = await this.repository.find({ user_id: userId });
+    await this.repository.remove(userTokens);
+  }
+}
