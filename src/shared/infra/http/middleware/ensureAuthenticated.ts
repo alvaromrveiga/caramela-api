17aed6582950cba036db176d7ffec389d78a9596
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-27 20:17:28 -0300

    Remove redundat user validation
    User validation already happens on the useCases

--- src/shared/infra/http/middleware/ensureAuthenticated.ts
+++ src/shared/infra/http/middleware/ensureAuthenticated.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 import { tokenSecret } from "../../../../config/auth";
-import { UsersRepository } from "../../../../modules/users/infra/typeorm/repositories/UsersRepository";
 
 export const ensureAuthenticated = async (
   req: Request,
@@ -19,20 +18,7 @@ export const ensureAuthenticated = async (
 
     const { sub: userId } = jwt.verify(token, tokenSecret) as { sub: string };
 
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(userId);
-
-    if (!user) {
-      res.status(401).json({ error: "Please Authenticate" });
-      return;
-    }
-
-    if (!isTokenValid(user.tokens, token)) {
-      res.status(401).json({ error: "Please Authenticate" });
-      return;
-    }
-
-    res.locals.userId = user.id;
+    res.locals.userId = userId;
     res.locals.token = token;
     // http://expressjs.com/en/api.html#res.locals
 
@@ -40,8 +26,4 @@ export const ensureAuthenticated = async (
   } catch (error) {
     res.status(401).json({ error: "Please Authenticate" });
   }
-};
-
-const isTokenValid = (tokens: string[], thisToken: string) => {
-  return tokens.find((token) => token === thisToken);
 };
