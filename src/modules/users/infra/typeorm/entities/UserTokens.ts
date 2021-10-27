35292fcd3e5a1bab9dc7753cbeece6a314b8cac4
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-27 15:57:35 -0300

    Init file

---
+++ src/modules/users/infra/typeorm/entities/UserTokens.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+import { User } from "./User";
+
+@Entity("users_tokens")
+export class UserTokens {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  refresh_token: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  user_id: string;
+
+  @Column()
+  expiration_date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
