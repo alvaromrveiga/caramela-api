afe99a49c2f80b54c29eca351bcee8983bf095e4
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-10-30 16:38:29 -0300

    Add way to receive token from request query
    To provide recovery password link

--- src/modules/users/useCases/refreshToken/RefreshTokenController.ts
+++ src/modules/users/useCases/refreshToken/RefreshTokenController.ts
@@ -5,7 +5,8 @@ import { RefreshTokenUseCase } from "./RefreshTokenUseCase";
 
 export class RefreshTokenController {
   async handle(req: Request, res: Response): Promise<Response> {
-    const refreshToken = req.body.refreshToken || req.headers["x-access-token"];
+    const refreshToken =
+      req.body.refreshToken || req.headers["x-access-token"] || req.query.token;
 
     const refreshTokenUseCase = container.resolve(RefreshTokenUseCase);
 
