9436ad734506c0a460c779d8671c0460297f978e
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-08 19:13:05 -0300

    Register appointments repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,14 +3,14 @@ import { container } from "tsyringe";
 import "./providers/StorageProvider";
 import "./providers/MailProvider";
 
+import { AppointmentsRepository } from "../../modules/appointments/infra/typeorm/repositories/AppointmentsRepository";
+import { IAppointmentsRepository } from "../../modules/appointments/repositories/IAppointmentsRepository";
 import { PetsRepository } from "../../modules/pets/infra/typeorm/repositories/PetsRepository";
 import { IPetsRepository } from "../../modules/pets/repositories/IPetsRepository";
 import { UsersRepository } from "../../modules/users/infra/typeorm/repositories/UsersRepository";
 import { UsersTokensRepository } from "../../modules/users/infra/typeorm/repositories/UsersTokensRepository";
 import { IUsersRepository } from "../../modules/users/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "../../modules/users/repositories/IUsersTokensRepository";
-
-container.registerSingleton<IPetsRepository>("PetsRepository", PetsRepository);
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
@@ -20,4 +20,11 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUsersTokensRepository>(
   "UsersTokensRepository",
   UsersTokensRepository
+);
+
+container.registerSingleton<IPetsRepository>("PetsRepository", PetsRepository);
+
+container.registerSingleton<IAppointmentsRepository>(
+  "AppointmentsRepository",
+  AppointmentsRepository
 );
