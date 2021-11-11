cd7c45fac931e250419ad5a83d3a38eb70259f7e
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-10 21:53:53 -0300

    Add show appointment route

--- src/shared/infra/http/routes/appointments.routes.ts
+++ src/shared/infra/http/routes/appointments.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateAppointmentController } from "../../../../modules/appointments/useCases/createAppointment/CreateAppointmentController";
+import { ShowAppointmentController } from "../../../../modules/appointments/useCases/showAppointment/ShowAppointmentController";
 import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
 
 const appointmentsRoutes = Router();
@@ -9,6 +10,12 @@ appointmentsRoutes.post(
   "/appointments",
   ensureAuthenticated,
   new CreateAppointmentController().handle
+);
+
+appointmentsRoutes.get(
+  "/appointments/:id",
+  ensureAuthenticated,
+  new ShowAppointmentController().handle
 );
 
 export { appointmentsRoutes };
