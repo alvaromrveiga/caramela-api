f98648eb4c62a29ca00abd50f6d45bac48d370a7
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-15 21:07:29 -0300

    Setup swagger docs route

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,7 +1,9 @@
 import "reflect-metadata";
 import "../../container";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerFile from "../../../swagger.json";
 import "express-async-errors";
 import createConnection from "../typeorm/connection";
 import { errorHandler } from "./middleware/errorHandler";
@@ -12,6 +14,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 app.use(errorHandler);
 
