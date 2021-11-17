fa2c9b23c0508a1be63cc33db22a3353c39bf831
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-17 16:24:54 -0300

    Comment formula source

--- src/shared/utils/getResetPasswordTokenSecret.ts
+++ src/shared/utils/getResetPasswordTokenSecret.ts
@@ -1,6 +1,7 @@
 import { User } from "../../modules/users/infra/typeorm/entities/User";
 
 export function getResetPasswordTokenSecret(user: User): string {
+  // https://www.smashingmagazine.com/2017/11/safe-password-resets-with-json-web-tokens/
   const hashedPassword = user.password;
 
   return hashedPassword + user.created_at.getTime();
