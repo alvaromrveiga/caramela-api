940d6b082008fe0bf42c12f36e370491a17027c4
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-17 20:37:36 -0300

    Set token to 15min and refreshToken to 180 days

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -9,10 +9,10 @@ if (!process.env.REFRESH_TOKEN_SECRET) {
   throw new NoRefreshTokenSecretError();
 }
 
-const tokenExpiresIn = "30m";
+const tokenExpiresIn = "15m";
 const tokenSecret = process.env.JWT_SECRET;
 
-const refreshTokenExpiresInDays = 30;
+const refreshTokenExpiresInDays = 180;
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
 
 const resetPasswordTokenExpiresInHours = 3;
