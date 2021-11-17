e2abd206d3f2deea6d03fada1c36d976c8a78640
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-17 16:12:28 -0300

    Remove unnused token

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -9,17 +9,12 @@ if (!process.env.REFRESH_TOKEN_SECRET) {
   throw new NoRefreshTokenSecretError();
 }
 
-if (!process.env.RESET_PASSWORD_TOKEN_SECRET) {
-  throw new NoRefreshTokenSecretError();
-}
-
 const tokenExpiresIn = "30m";
 const tokenSecret = process.env.JWT_SECRET;
 
 const refreshTokenExpiresInDays = 30;
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
 
-const resetPasswordTokenSecret = process.env.RESET_PASSWORD_TOKEN_SECRET;
 const resetPasswordTokenExpiresInHours = 3;
 
 export {
@@ -27,6 +22,5 @@ export {
   tokenSecret,
   refreshTokenExpiresInDays,
   refreshTokenSecret,
-  resetPasswordTokenSecret,
   resetPasswordTokenExpiresInHours,
 };
--- src/config/errors/NoResetPasswordTokenError.ts
+++
@@ -1,7 +0,0 @@
-import { AppError } from "../../shared/errors/AppError";
-
-export class NoResetPasswordTokenSecretError extends AppError {
-  constructor() {
-    super("No RESET_PASSWORD_TOKEN_SECRET defined on .env", 500);
-  }
-}
