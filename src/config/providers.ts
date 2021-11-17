6495ee520345c213f2e4237c7a0589308afbf23b
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-17 16:25:33 -0300

    Add providers configurations

---
+++ src/config/providers.ts
@@ -0,0 +1,11 @@
+// Which mail provider to use
+// Currently has only ethereal
+const mailProviders = ["ethereal"];
+const MAIL_PROVIDER = mailProviders[0];
+
+// Where to store the files
+// currently has only local storage
+const storageProviders = ["local"];
+const STORAGE_PROVIDER = storageProviders[0];
+
+export { MAIL_PROVIDER, STORAGE_PROVIDER };
