9a2f102c590a3e60eb66366740fddcbf876bb479
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-22 16:56:12 -0300

    Use deploy database info when needed

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,13 +1,38 @@
-module.exports = {
+import { parse } from "pg-connection-string";
+
+import { isDeploy } from "./src/shared/utils/isDeploy";
+
+const ormConfig = {
   type: "postgres",
   host: "localhost",
   port: "5432",
   username: "postgres",
   password: "docker",
   database: "caramela",
+  ssl: false,
+  extra: {},
   migrations: ["./src/shared/infra/typeorm/migrations/**.ts"],
   entities: ["./src/modules/**/entities/**.ts"],
   cli: {
     migrationsDir: "./src/shared/infra/typeorm/migrations",
   },
 };
+
+const databaseURL = isDeploy();
+if (databaseURL) {
+  const databaseConfig = parse(databaseURL);
+
+  ormConfig.host = databaseConfig.host || ormConfig.host;
+  ormConfig.port = databaseConfig.port || ormConfig.port;
+  ormConfig.username = databaseConfig.user || ormConfig.username;
+  ormConfig.password = databaseConfig.password || ormConfig.password;
+  ormConfig.database = databaseConfig.database || ormConfig.database;
+  ormConfig.ssl = true;
+  ormConfig.extra = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+
+module.exports = { ...ormConfig };
--- src/shared/infra/typeorm/connection.ts
+++ src/shared/infra/typeorm/connection.ts
@@ -1,5 +1,7 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
+import { isDeploy } from "../../utils/isDeploy";
+
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
@@ -9,6 +11,8 @@ export default async (): Promise<Connection> => {
   if (process.env.NODE_ENV === "test") {
     host = "localhost";
     database = "caramela_test";
+  } else if (isDeploy()) {
+    return createConnection(defaultOptions);
   } else {
     host = "database-caramela";
     database = "caramela";
