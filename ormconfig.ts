75a8ba607de90af2ecefccb77bfb5fc2d91a5dbc
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-23 14:46:52 -0300

    Use database config file

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,24 +1,11 @@
-const ormConfig = {
+import { databaseConfig } from "./src/config/database";
+
+module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
   ssl: false,
-  extra: {},
-  migrations: ["./src/shared/infra/typeorm/migrations/**.ts"],
-  entities: ["./src/modules/**/entities/**.ts"],
-  cli: {
-    migrationsDir: "./src/shared/infra/typeorm/migrations",
-  },
+  extra: databaseConfig.extra,
+  migrations: databaseConfig.migrations,
+  entities: databaseConfig.entities,
+  cli: databaseConfig.cli,
 };
-
-if (process.env.NODE_ENV === "prod") {
-  ormConfig.ssl = true;
-  ormConfig.extra = {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  };
-  ormConfig.migrations = ["./dist/src/shared/infra/typeorm/migrations/**.js"];
-  ormConfig.entities = ["./dist/src/modules/**/entities/**.js"];
-}
-
-module.exports = { ...ormConfig };
