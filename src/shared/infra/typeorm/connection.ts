6e18dbf2b3da840d763a004ef924b2003fef3e99
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-23 14:32:40 -0300

    Check if env is prod

--- src/shared/infra/typeorm/connection.ts
+++ src/shared/infra/typeorm/connection.ts
@@ -1,7 +1,5 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-import { isDeploy } from "../../utils/isDeploy";
-
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
@@ -11,7 +9,7 @@ export default async (): Promise<Connection> => {
   if (process.env.NODE_ENV === "test") {
     host = "localhost";
     database = "caramela_test";
-  } else if (isDeploy()) {
+  } else if (process.env.NODE_ENV === "prod") {
     return createConnection(defaultOptions);
   } else {
     host = "database-caramela";
