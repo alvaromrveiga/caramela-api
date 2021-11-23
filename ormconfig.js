f5518a91acb8ad31467e00937f632fe727123570
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-23 16:10:59 -0300

    Remove if databaseUrl exists

--- ormconfig.js
+++ ormconfig.js
@@ -1,35 +1,33 @@
 const pgConnectionString = require("pg-connection-string");
 
-if (process.env.DATABASE_URL) {
-  const database = pgConnectionString.parse(process.env.DATABASE_URL);
+const databaseConfig = pgConnectionString.parse(process.env.DATABASE_URL);
 
-  const ormConfig = {
-    type: "postgres",
-    host: database.host,
-    port: database.port,
-    username: database.user,
-    password: database.password,
-    database: database.database,
-    ssl: false,
-    extra: {},
-    migrations: ["./src/shared/infra/typeorm/migrations/**.ts"],
-    entities: ["./src/modules/**/entities/**.ts"],
-    cli: {
-      migrationsDir: "./src/shared/infra/typeorm/migrations",
+const ormConfig = {
+  type: "postgres",
+  host: databaseConfig.host,
+  port: databaseConfig.port,
+  username: databaseConfig.user,
+  password: databaseConfig.password,
+  database: databaseConfig.database,
+  ssl: false,
+  extra: {},
+  migrations: ["./src/shared/infra/typeorm/migrations/**.ts"],
+  entities: ["./src/modules/**/entities/**.ts"],
+  cli: {
+    migrationsDir: "./src/shared/infra/typeorm/migrations",
+  },
+};
+
+if (process.env.NODE_ENV === "prod") {
+  ormConfig.ssl = true;
+  ormConfig.extra = {
+    ssl: {
+      rejectUnauthorized: false,
     },
   };
-
-  if (process.env.NODE_ENV === "prod") {
-    ormConfig.ssl = true;
-    ormConfig.extra = {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    };
-    ormConfig.migrations = ["./dist/src/shared/infra/typeorm/migrations/**.js"];
-    ormConfig.entities = ["./dist/src/modules/**/entities/**.js"];
-    ormConfig.cli.migrationsDir = ".dist/src/shared/infra/typeorm/migrations";
-  }
-
-  module.exports = { ...ormConfig };
+  ormConfig.migrations = ["./dist/src/shared/infra/typeorm/migrations/**.js"];
+  ormConfig.entities = ["./dist/src/modules/**/entities/**.js"];
+  ormConfig.cli.migrationsDir = ".dist/src/shared/infra/typeorm/migrations";
 }
+
+module.exports = { ...ormConfig };
