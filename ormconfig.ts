5b12d2ec54941a705c598e6d01833aa5eb6e2d5c
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-23 14:32:19 -0300

    Get database url by env DATABASE_URL

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,6 @@
-import { parse } from "pg-connection-string";
-
-import { isDeploy } from "./src/shared/utils/isDeploy";
-
 const ormConfig = {
   type: "postgres",
-  host: "localhost",
-  port: "5432",
-  username: "postgres",
-  password: "docker",
-  database: "caramela",
+  url: process.env.DATABASE_URL,
   ssl: false,
   extra: {},
   migrations: ["./src/shared/infra/typeorm/migrations/**.ts"],
@@ -18,15 +10,7 @@ const ormConfig = {
   },
 };
 
-const databaseURL = isDeploy();
-if (databaseURL) {
-  const databaseConfig = parse(databaseURL);
-
-  ormConfig.host = databaseConfig.host || ormConfig.host;
-  ormConfig.port = databaseConfig.port || ormConfig.port;
-  ormConfig.username = databaseConfig.user || ormConfig.username;
-  ormConfig.password = databaseConfig.password || ormConfig.password;
-  ormConfig.database = databaseConfig.database || ormConfig.database;
+if (process.env.NODE_ENV === "prod") {
   ormConfig.ssl = true;
   ormConfig.extra = {
     ssl: {
