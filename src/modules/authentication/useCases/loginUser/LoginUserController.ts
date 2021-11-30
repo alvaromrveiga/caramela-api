1d3cbfa39e7e94e18fbdc2480425cd65b538c33e
Álvaro Veiga <alvaromrveiga@gmail.com>
2021-11-30 11:15:53 -0300

    Get machineInfo from request headers

--- src/modules/authentication/useCases/loginUser/LoginUserController.ts
+++ src/modules/authentication/useCases/loginUser/LoginUserController.ts
@@ -5,7 +5,13 @@ import { LoginUserUseCase } from "./LoginUserUseCase";
 
 export class LoginUserController {
   async handle(req: Request, res: Response): Promise<Response> {
-    const { email, password, machineInfo } = req.body;
+    const { email, password } = req.body;
+
+    const machineInfo =
+      `${req.ip} ${req.headers["user-agent"]} ${req.headers["accept-language"]}`.replace(
+        / undefined/g,
+        ""
+      );
 
     const loginUserUseCase = container.resolve(LoginUserUseCase);
 
